2bb125b0006cae3ce8502af135da2752e4ad2b06
Archie Angeles <archieangeles@gmail.com>
2019-01-17 00:05:34 +0800

    Initial Commit

---
+++ ServiceRatings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ServiceRatings
+//
+//  Created by Archimedes Angeles on 17/1/19.
+//  Copyright © 2019 Archimedes Angeles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
